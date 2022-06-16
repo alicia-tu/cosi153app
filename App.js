82443f83f18e1a66917775a9186f475f0a18262c
alicia-tu <tujy@brandeis.edu>
2022-06-16 09:46:28 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Image, StyleSheet, Text, Button, TextInput} from 'react-native';
+import picture from './alicia.jpg';
 
-export default function App() {
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    backgroundColor: "powderblue",
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  image:{
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title:{
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 100,
+  },
+  text:{
+    fontSize: 40,
+    textAlign: "center",
+  }
+});
+
+const DisplayAnImage = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!
-        Happy!
-      </Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.title}>About the developer </Text>
+      <Text style = {styles.text}>Alicia Tu</Text>
+      <View style = {styles.image}>
+      <Image
+        style={styles.logo}
+        source={picture}
+      />
+      </View>
+      <Text style = {styles.text}>I am a rising senior majoring in Computer Science, Linguistics, and Classical Studies</Text>
+      <Text style = {[styles.text, {fontWeight:"bold"}]}> {'\n'} App Description:</Text>
+      <Text style = {styles.text}>I want to help people to know about more subjects and encourage them to explore more in other projects
+      I want to make an app that display the most common majors in college, and give out information on major description, as well as possible future career.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default DisplayAnImage;
